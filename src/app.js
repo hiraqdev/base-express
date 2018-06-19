ba6b3ae651f9e6e553c93bcdb7989795d457a38e
hiraq <hrxwan@gmail.com>
2018-06-19 11:37:28 +0700

    Setup standardjs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.get('/', (req, res) => res.send('Hello world'));
+app.get('/', (req, res) => res.send('Hello world'))
 
-app.listen(3000, () => console.log('testing'));
+app.listen(3000, () => console.log('testing'))
