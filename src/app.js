eb01c8d295d3c8604f28c37686b918f43c820059
hiraq <hrxwan@gmail.com>
2018-06-19 13:16:55 +0700

    Setup dotenv & middlewares
    Configure port and host via dotenv file & setup
    bodyParser & morgan middlewares.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello world'))
+// setup bodyparser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.listen(3000, () => console.log('testing'))
+// setup morgan
+app.use(morgan('common'))
+
+// run express engine
+app.listen(process.env.PORT, process.env.HOST, () => {
+  console.log('=========================')
+  console.log(`Port: ${process.env.PORT}`)
+  console.log(`Host: ${process.env.HOST}`)
+  console.log('=========================')
+})
